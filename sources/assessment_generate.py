69bcfb4bdb1e7722699d94f6870d879ed59f73af
kriyanation <homeofmars@gmail.com>
2020-05-11 17:03:10 +0530

    MVP 1.1

--- sources/assessment_generate.py
+++ sources/assessment_generate.py
@@ -1,24 +1,30 @@
-import reportlab
+import logging
+import traceback
+
 from reportlab.pdfgen import canvas
-import data_capture
+import data_capture_assessment
+from reportlab.pdfgen import canvas
 
+import data_capture_assessment
 
-
-
+logger = logging.getLogger("MagicLogger")
 def generate_ip_paper(lesson_id,filename,db):
-    data_capture.TEST_ROW = lesson_id
-    text_id = data_capture.get_ip_data(db)
-    text = text_id[1]
-    id = text_id[2]
-    title = text_id[0]
-    cc = canvas.Canvas(filename)
-    cc.setTitle("Learning Assessment")
-    cc.setFont("Helvetica", 16)
-    cc.drawCentredString(300,820,"A Learning Assessment for "+title)
-    cc.setFont("Helvetica", 10)
-    textobject = cc.beginText()
-    textobject.setTextOrigin(50, 800)
-    textobject.textLines(text)
-    cc.drawText(textobject)
-    cc.showPage()
-    cc.save()
+    try:
+        data_capture_assessment.TEST_ROW = lesson_id
+        text_id = data_capture_assessment.get_ip_data(db)
+        text = text_id[1]
+        id = text_id[2]
+        title = text_id[0]
+        cc = canvas.Canvas(filename)
+        cc.setTitle("Learning Assessment")
+        cc.setFont("Helvetica", 16)
+        cc.drawCentredString(300,820,"A Learning Assessment for "+title)
+        cc.setFont("Helvetica", 10)
+        textobject = cc.beginText()
+        textobject.setTextOrigin(50, 800)
+        textobject.textLines(text)
+        cc.drawText(textobject)
+        cc.showPage()
+        cc.save()
+    except:
+        logger.error(traceback.print_exc())
