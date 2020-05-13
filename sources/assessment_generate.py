d423efbe73789f9ffb2fe79eed7a91da965ec9fd
kriyanation <homeofmars@gmail.com>
2020-05-13 23:03:51 +0530

    MVP 1.1

--- sources/assessment_generate.py
+++ sources/assessment_generate.py
@@ -4,6 +4,7 @@ import traceback
 from reportlab.pdfgen import canvas
 import data_capture_assessment
 from reportlab.pdfgen import canvas
+from textwrap import wrap
 
 import data_capture_assessment
 
@@ -20,9 +21,10 @@ def generate_ip_paper(lesson_id,filename,db):
         cc.setFont("Helvetica", 16)
         cc.drawCentredString(300,820,"A Learning Assessment for "+title)
         cc.setFont("Helvetica", 10)
+        wraped_text = "\n".join(wrap(text, 80))
         textobject = cc.beginText()
         textobject.setTextOrigin(50, 800)
-        textobject.textLines(text)
+        textobject.textLines(wraped_text)
         cc.drawText(textobject)
         cc.showPage()
         cc.save()
