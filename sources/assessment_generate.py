5daabbc26a357a6e2629c0348dc12640f180f70f
Ramprasad <homeofmars@gmail.com>
2020-06-05 00:28:35 +0530

    MVP - Full

--- sources/assessment_generate.py
+++ sources/assessment_generate.py
@@ -6,10 +6,13 @@ import data_capture_assessment
 from reportlab.pdfgen import canvas
 from textwrap import wrap
 
+
 import data_capture_assessment
 
 logger = logging.getLogger("MagicLogger")
 def generate_ip_paper(lesson_id,filename,db):
+    print("Inside Assessment Generation")
+    print(filename)
     try:
         data_capture_assessment.TEST_ROW = lesson_id
         text_id = data_capture_assessment.get_ip_data(db)
@@ -19,9 +22,17 @@ def generate_ip_paper(lesson_id,filename,db):
         title = text_id[0]
         cc = canvas.Canvas(filename)
         cc.setTitle("Learning Assessment")
+        cc.setFont("Helvetica", 8)
+        cc.setFillColorRGB(0,0,1) 
+        cc.drawCentredString(300,810,"http://www.wondersky.in")
+        cc.setFillColorRGB(0,0,0) 
         cc.setFont("Helvetica", 16)
-        cc.drawCentredString(300,820,"A Learning Assessment for "+title)
+        cc.drawCentredString(300,820,title)
+        
         cc.setFont("Helvetica", 10)
+        link_width = cc.stringWidth("http://www.wondersky.in")
+        link_rect = (220,810,290,795)
+        cc.linkURL("http://www.wondersky.in", link_rect)
         wraped_text = "\n".join(wrap(text, 90,replace_whitespace=False))
         textobject = cc.beginText()
         textobject.setTextOrigin(50, 800)
@@ -31,4 +42,4 @@ def generate_ip_paper(lesson_id,filename,db):
         cc.save()
     except:
         print("Assessment paper generation failed")
-        logger.error(traceback.print_exc())
+        logger.exception("Assessment Generation Error")
